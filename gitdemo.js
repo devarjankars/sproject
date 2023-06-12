88ad4919f0a932f3b4640e80d2bfe8b2289be33c
devarjankars <devarjankars@gmail.com>
2023-06-13 00:40:11 +0530

    added

---
+++ gitdemo.js
@@ -0,0 +1,7 @@
+let ul=document.getElementById("header-title");
+ul.textContent="HELLO world";
+let c=document.getElementsByClassName('list-group-item');
+c[0].style.fontWeight="bold";
+c[1].style.fontWeight="bold";
+c[3].style.fontWeight="bold";
+c[2].style.backgroundColor="green";
