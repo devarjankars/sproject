949874d9e2b06c739d882a0f15efadd687fe92a1
devarjankars <devarjankars@gmail.com>
2023-06-13 23:09:25 +0530

    add

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
  let tag=document.getElementsByTagName("li");
  tag[4].innerText="helloTag";
  tag[4].style.backgroundColor="lightblue";
+ let items=document.querySelectorAll('.list-group-item');
+ items[1].style.color="green";
+ let odd=document.querySelectorAll('.list-group-item:nth-child(odd)');
+ for(let i=0;i<odd.length;i++)
+odd[i].style.backgroundColor="green";
+
+let inv=document.querySelectorAll(".list-group-item")
+inv[2].style.display="none";
